5b186534d517ae31a4ede0d06a5341540ba8e9af
omerkynkc54 <omerkay54@gmail.com>
2024-01-16 19:13:06 +0300

    added constants

---
+++ MyStockMauiBlazor/Models/StockSummary.cs
@@ -0,0 +1,10 @@
+﻿namespace MyStockMauiBlazor.Models
+{
+    public class StockSummary
+    {
+        public string StockCode { get; set; }
+        public double AvgBuyPrice { get; set; }
+        public double UserStockAmount { get; set; }
+        public double ProfitLoss { get; set; }
+    }
+}
---
+++ MyStockMauiBlazor/Services/DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace MyStockMauiBlazor.Services
+{
+    public class DatabaseService
+    {
+        SQLiteAsyncConnection Database;
+        public DatabaseService()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database != null)
+                return;
+
+            // Assuming Constants.DatabasePath and Constants.Flags are defined
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            await Database.CreateTableAsync<User>();
+        }
+
+    }
+}
